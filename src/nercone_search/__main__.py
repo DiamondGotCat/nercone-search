98a81b0d342b7cd273fdad643ba78e2ec2a11e5c
DiamondGotCat <chii.2019.5555@gmail.com>
2026-01-03 18:32:01 +0900

    --

--- src/nercone_search/__main__.py
+++ src/nercone_search/__main__.py
@@ -37,7 +37,7 @@ def main():
     subparser = parser.add_subparsers(dest="command", required=True)
 
     cmd_init = subparser.add_parser("init", help="データーベースを初期化")
-    cmd_init.add_argument("exist_ok", type=str_to_bool, default=True, help="既に必要なテーブルが存在する場合にスキップするかどうか")
+    cmd_init.add_argument("--exist_ok", type=str_to_bool, default=True, help="既に必要なテーブルが存在する場合にスキップするかどうか")
     cmd_init.set_defaults(func=_cmd_init)
 
     cmd_serve = subparser.add_parser("serve", help="APIサーバーを起動")
@@ -45,13 +45,13 @@ def main():
 
     cmd_crawl = subparser.add_parser("crawl", help="指定したサイトをクロール")
     cmd_crawl.add_argument("url", type=str, help="クロールするページのURL")
-    cmd_crawl.add_argument("recursive", type=str_to_bool, default=True, help="サイト内にあるリンクをさらにクロールするかどうか")
-    cmd_crawl.add_argument("disallow_ok", type=str_to_bool, default=True, help="robots.txtで拒否された場合に例外を発生させないかどうか")
+    cmd_crawl.add_argument("--recursive", type=str_to_bool, default=True, help="サイト内にあるリンクをさらにクロールするかどうか")
+    cmd_crawl.add_argument("--disallow_ok", type=str_to_bool, default=True, help="robots.txtで拒否された場合に例外を発生させないかどうか")
     cmd_crawl.set_defaults(func=_cmd_crawl)
 
     cmd_search = subparser.add_parser("search", help="キーワードから検索")
     cmd_search.add_argument("query", type=str, help="クエリ")
-    cmd_search.add_argument("nums", type=int, default=50, help="検索結果数の上限")
+    cmd_search.add_argument("--nums", type=int, default=50, help="検索結果数の上限")
     cmd_search.set_defaults(func=_cmd_search)
 
     args = parser.parse_args()
